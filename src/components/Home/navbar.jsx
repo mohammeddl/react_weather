0066bb66f78dcc09c29e112069fbcc67ff0fe434
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-04-16 14:56:23 +0100

    end v.1

--- src/components/Home/navbar.jsx
+++ src/components/Home/navbar.jsx
@@ -25,10 +25,10 @@ export default function NavBar() {
         <div className='flex-1'>
           <ul className='pt-2 pb-4 space-y-1 text-sm'>
             <li className='rounded-sm'>
-              <a
+              <div
                 rel='noopener noreferrer'
                 href='#'
-                className='flex items-center text-white font-semibold p-2 space-x-3 rounded-md'>
+                className='flex items-center text-white px-2 bg-gray-500 bg-opacity-25   font-semibold p-1 space-x-3 rounded-md'>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   height='14'
@@ -40,26 +40,10 @@ export default function NavBar() {
                     d='M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z'
                   />
                 </svg>
-                <span className={`${mobileNav ? "hidden" : "block"}`}>
+                <span className={`${mobileNav ? "hidden" : "block"} `}>
                 Weather
                 </span>
-              </a>
-            </li>
-            <li className='rounded-sm dark:bg-gray-100 dark:text-gray-900'>
-              <a
-                rel='noopener noreferrer'
-                href='#'
-                className='flex items-center p-2 space-x-3 rounded-md'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 512 512'
-                  className='w-5 h-5 fill-current dark:text-gray-600'>
-                  <path d='M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z'></path>
-                </svg>
-                <span className={`${mobileNav ? "hidden" : "block"}`}>
-                  settings
-                </span>
-              </a>
+              </div>
             </li>
           </ul>
         </div>
